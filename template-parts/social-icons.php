d56682a4f38ad4bbda77ea570e2dfa4d377f20e8
Maksimilian Rogulin <maksimilianrogulin@gmail.com>
2026-03-04 13:33:16 +0300

    changed url to max messenger

--- template-parts/social-icons.php
+++ template-parts/social-icons.php
@@ -1,12 +1,6 @@
 <div class="social-icons">
-    <!-- <a class="social-icons__link social-icons__link--wa"
-       href="https://wa.me/79295401177"
-       target="_blank"
-       rel="noopener">
-        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/whatsapp.png'; ?>" alt="Logo" width="32" height="32"/>
-    </a> -->
     <a class="social-icons__link social-icons__link--max"
-       href="https://max.ru[79295401177]"
+       href="https://max.ru/u/f9LHodD0cOJzsc2z6l1hVbLSFX9YtkIjhpwPDRcIJcT5ma0IZs2uYwx8Phc"
        target="_blank"
        rel="noopener">
         <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/max.png'; ?>" alt="Logo" width="32" height="32"/>
